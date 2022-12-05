df5eee3f753cbc874f6b7a3d47044db4fe39be7c
Matthew Adcock <43476212+MattA-Official@users.noreply.github.com>
2022-12-05 23:13:08 +0000

    Day 5 - Part 2

--- Day 5/Program.cs
+++ Day 5/Program.cs
@@ -52,6 +52,50 @@ Finally, one crate is moved from stack 1 to stack 2:
 The Elves just need to know which crate will end up on top of each stack; in this example, the top crates are C in stack 1, M in stack 2, and Z in stack 3, so you should combine these together and give the Elves the message CMZ.
 
 After the rearrangement procedure completes, what crate ends up on top of each stack?
+
+--- Part Two ---
+As you watch the crane operator expertly rearrange the crates, you notice the process isn't following your prediction.
+
+Some mud was covering the writing on the side of the crane, and you quickly wipe it away. The crane isn't a CrateMover 9000 - it's a CrateMover 9001.
+
+The CrateMover 9001 is notable for many new and exciting features: air conditioning, leather seats, an extra cup holder, and the ability to pick up and move multiple crates at once.
+
+Again considering the example above, the crates begin in the same configuration:
+
+    [D]
+[N] [C]
+[Z] [M] [P]
+ 1   2   3
+Moving a single crate from stack 2 to stack 1 behaves the same as before:
+
+[D]
+[N] [C]
+[Z] [M] [P]
+ 1   2   3
+However, the action of moving three crates from stack 1 to stack 3 means that those three moved crates stay in the same order, resulting in this new configuration:
+
+        [D]
+        [N]
+    [C] [Z]
+    [M] [P]
+ 1   2   3
+Next, as both crates are moved from stack 2 to stack 1, they retain their order as well:
+
+        [D]
+        [N]
+[C]     [Z]
+[M]     [P]
+ 1   2   3
+Finally, a single crate is still moved from stack 1 to stack 2, but now it's crate C that gets moved:
+
+        [D]
+        [N]
+        [Z]
+[M] [C] [P]
+ 1   2   3
+In this example, the CrateMover 9001 has put the crates in a totally different order: MCD.
+
+Before the rearrangement process finishes, update your simulation so that the Elves know where they should stand to be ready to unload the final supplies. After the rearrangement procedure completes, what crate ends up on top of each stack?
 */
 
 namespace Day_5
@@ -159,6 +203,74 @@ namespace Day_5
 
             // Print the answer
             Console.WriteLine("Part 1: " + answer);
+
+            // Reset the stacks
+            for (int i = 0; i < stacks.Count; i++)
+            {
+                stacks[i] = new Stack<char>();
+            }
+
+            // Loop through the input backwards to get the stacks
+            for (int i = input.Length - 1; i >= 0; i--)
+            {
+                string line = input[i];
+
+                if (line != "" && !line.StartsWith("move"))
+                {
+                    // Add every third character starting with the second one to the appropriate stack
+                    for (int j = 1; j < line.Length; j += 4)
+                    {
+                        // if the character is a space or a number, then skip it
+                        if (line[j] == ' ' || (line[j] >= '0' && line[j] <= '9'))
+                        {
+                            continue;
+                        }
+
+                        stacks[((j - 1) / 4)].Push(line[j]);
+                    }
+                }
+            }
+
+            // Loop through the moves preserving the order
+            for (int i = 0; i < moves.Count; i++)
+            {
+                // Get the move
+                Move move = moves[i];
+
+                // Create a temporary list to hold the crates
+                List<char> crates = new List<char>();
+
+                // Loop through the crates to move
+                for (int j = 0; j < move.Quantity; j++)
+                {
+                    // Add the crate to the list
+                    crates.Add(stacks[move.From - 1].Pop());
+                }
+
+                // Add the crates to the stack
+                for (int j = crates.Count - 1; j >= 0; j--)
+                {
+                    stacks[move.To - 1].Push(crates[j]);
+                }
+            }
+
+            // Reset the answer
+            answer = "";
+
+            // Loop through the stacks
+            for (int i = 0; i < stacks.Count; i++)
+            {
+                // If the stack is empty, then skip it
+                if (stacks[i].Count == 0)
+                {
+                    continue;
+                }
+
+                // Add the top crate to the answer
+                answer += stacks[i].Peek();
+            }
+
+            Console.WriteLine("Part 2: " + answer);
 
             Console.ReadLine();
         }
